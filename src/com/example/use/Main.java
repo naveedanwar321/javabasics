38a1fd57c6917003f2d6a3bc606548a154ee8c41
Muhammad Ali Qasmi <aqasmi@etilizepak.com>
2018-01-24 11:04:05 +0500

    - Issues fixed.

--- src/com/example/use/Main.java
+++ src/com/example/use/Main.java
@@ -5,6 +5,8 @@ package com.example.use;
 
 import com.example.hello.HelloWorld;
 import com.example.hello.HelloWorldImpl;
+import com.simple.calculator.SimpleCalculator;
+import com.simple.calculator.SimpleCalculatorImpl;
 
 /**
  * 
@@ -17,6 +19,8 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        
+        // Class Work 
         
         HelloWorld myObject = new HelloWorldImpl();
         
@@ -38,6 +42,24 @@ public class Main {
         
         String fullName = myObject.getFullName(firstName, lastName);
         System.out.println(fullName);
+                
+        
+        // Home Work
+        
+        SimpleCalculator simpleCalculator = new SimpleCalculatorImpl();
+        
+        int n = simpleCalculator.addTwoNumbers(1, 3);
+        System.out.println(n);
+        
+        int m = simpleCalculator.subtractTwoNumbers(1, 3);
+        System.out.println(m);
+        
+        int o = simpleCalculator.multiplyTwoNumbers(1, 3);
+        System.out.println(o);
+        
+        int p = simpleCalculator.divideTwoNumbers(3, 4);
+        System.out.println(p);
+        
     }
 
 }
